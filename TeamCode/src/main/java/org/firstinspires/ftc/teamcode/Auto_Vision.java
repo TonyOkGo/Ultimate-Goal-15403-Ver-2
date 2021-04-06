1d1c5b2f9095e803629c09ab2d1885593c41890a
Tony Ortiz <ortiz.antonio22@libertycommon.org>
2021-04-06 17:13:19 -0600

    give bennet the COMEMTNS on the code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto_Vision.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto_Vision.java
@@ -13,22 +13,38 @@ public class Auto_Vision extends Auto_Util {
         //Line up to the rings in order to scan
         encoderStrafe(STRAFE_SPEED, -5,-5,10,0);
         encoderDrive(DRIVE_SPEED, -25,-25,10,0);
-        //funny scan vision
-        if(amountofrings == 0){
+        amountofrings = ub_vision();
+        //funny scan vision to get the amountofrings integer
 
-        }
-        if(amountofrings ==1){
-
-        }
-        if(amountofrings == 2){
-            
-        }
+        //Drive to the line and shoot the rings into the goal
         encoderStrafe(STRAFE_SPEED, -15,-24,10,0);
         encoderDrive(DRIVE_SPEED, -30,-30,10,0);
         encoderStrafe(STRAFE_SPEED, 22,22,10,0);
         encoderDrive(DRIVE_SPEED, -7,-7,10,0);
         encoderDrive(DRIVE_SPEED, -2.2,2.2,10,0);
         shoot(5);
+        if(amountofrings == 0){
+            //strafe left to be lined up with the first square
+            //drop the arm motor
+            //open the arm servo
+            //drive backwards
+        }
+        if(amountofrings ==1){
+            //strafe left to be lined up with the second square
+            //drive forward to be lined up with that square
+            //drop the arm motor
+            //open the arm servo
+            //drive backwards
+        }
+        if(amountofrings == 2){
+            //strafe left to be lined up with the third square
+            //drive forward to be lined up with that square
+            //drop the arm motor
+            //open the arm servo
+            //drive backwards
+
+        }
+
         telemetry.addData("fsda","fsdaf");
         telemetry.update();
 
