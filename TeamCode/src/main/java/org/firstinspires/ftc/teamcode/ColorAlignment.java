fff2f690bd739f9d2c99e30e74457d80cced70a7
bennetklaver <klaver.bennet22@libertycommon.org>
2021-04-20 16:13:38 -0600

    Teleop functionality to ColorAlignment

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ColorAlignment.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ColorAlignment.java
@@ -1,4 +1,12 @@
 package org.firstinspires.ftc.teamcode;
 
-public class ColorAlignment {
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp (name="ColorAlignment Test", group="Sensor Test")
+public class ColorAlignment extends LinearOpMode {
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+    }
 }
